f6b20b524dc1d6e94edff0a5eb7336ddf748aa23
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-26 07:01:28 +0800

    Program Instructions and Pseudocode

---
+++ calculate_income_tax_for_the_given_income_by_adhering_to_the_rules_below.py
@@ -0,0 +1,6 @@
+# Assignment 5 - Exercise 12
+# Calculate income tax for the given income by adhering to the rules below
+
+# Request income from user
+# Use else-if for formula and conditions
+# Print output
