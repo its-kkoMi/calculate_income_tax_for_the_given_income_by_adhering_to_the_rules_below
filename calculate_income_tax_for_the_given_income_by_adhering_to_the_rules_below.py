371e384f88b6964de36de128ac0cc0c7e9d7967e
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-26 07:03:38 +0800

    Added a user input

--- calculate_income_tax_for_the_given_income_by_adhering_to_the_rules_below.py
+++ calculate_income_tax_for_the_given_income_by_adhering_to_the_rules_below.py
@@ -2,5 +2,9 @@
 # Calculate income tax for the given income by adhering to the rules below
 
 # Request income from user
+
+income = int(input("Enter your income: "))
+tax_amount = 0
+
 # Use else-if for formula and conditions
 # Print output
