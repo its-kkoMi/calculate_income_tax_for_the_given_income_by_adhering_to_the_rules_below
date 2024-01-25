ec5e4583d8b03bd50102a268516cbe1ded8919d9
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-26 07:11:39 +0800

    Added else-if and output

--- calculate_income_tax_for_the_given_income_by_adhering_to_the_rules_below.py
+++ calculate_income_tax_for_the_given_income_by_adhering_to_the_rules_below.py
@@ -7,4 +7,24 @@ income = int(input("Enter your income: "))
 tax_amount = 0
 
 # Use else-if for formula and conditions
+
+if income <= 10000:
+    tax_amount = 0
+    
+elif income <= 20000:
+    income_above_10000 = income - 10000
+    tax_amount = income_above_10000 * 10 / 100
+    
+else:
+    # First 10k, no tax
+    tax_amount = 0
+    
+    # Second 10k, 10% tax
+    tax_amount = 10000 * 10 / 100  
+    
+    # Remaining amount, 20% tax
+    tax_amount += (income - 20000) * 20 / 100
+
 # Print output
+
+print("Total tax to pay is", tax_amount)
